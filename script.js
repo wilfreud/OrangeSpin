ec2c8516e243a1127860a082e0db01543befb63c
wilfreud <killlergangster@gmail.com>
2021-12-02 21:10:01 +0000

    Added easter egg for spining all at the same time

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const orangebox = document.getElementById("orangebox");
-console.log(orangebox);
 const orangeCount = 180;
 
 // orangebox gridding settings
 orangebox.style.gridTemplateColumns = "repeat(20, 1fr)";
 
-function orangeCreator(){
+function orangeCreator(orangeId){
     let imgCreate = document.createElement("img");
     imgCreate.src = "orange.svg";
     imgCreate.title = "Click to spin";
     imgCreate.classList.add("orange");
     orangebox.appendChild(imgCreate);
 
-    imgCreate.addEventListener("click", () => {
-        imgCreate.classList.add("animate");
-        imgCreate.addEventListener("animationend", () => {imgCreate.classList.remove("animate");})
+    if (orangeId === Math.round(orangeCount/2)) {imgCreate.classList.add("triggeredOrange");}
+    animeIt(imgCreate);
+
+}
+
+function animeIt(itemAnim){
+        itemAnim.addEventListener("click", () => {
+        itemAnim.classList.add("animate");
+        itemAnim.addEventListener("animationend", () => {itemAnim.classList.remove("animate");})
     })
 }
 
-for (let index = 0; index < orangeCount; index++) {orangeCreator();}
+for (let index = 0; index < orangeCount; index++) {orangeCreator(index);}
+
+document.querySelector(".triggeredOrange").addEventListener("click", () => {
+    let belugaPass = prompt("Enter Secret cheat code: ");
+    if (belugaPass === "spinAll") {
+        document.querySelectorAll(".orange").forEach(element => {
+            element.classList.add("animate")
+            element.addEventListener("animationend", () => {element.classList.remove("animate");})
+        });
+    }
+    else{alert("Unknown cheat code");}
+})
